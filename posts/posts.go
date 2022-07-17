336ba6181553b85d7f44dcd1e8a50ad30b7cb135
Balazs Kezes <3j4b0y1i@anonaddy.me>
2022-07-17 06:51:17 +0200

    comments: define a const for the cooldown period

--- posts/posts.go
+++ posts/posts.go
@@ -24,6 +24,8 @@ import (
 	"unsafe"
 )
 
+const commentCooldownMS = 3 * 60000
+
 var postPath = flag.String("postpath", ".", "path to the posts")
 var createdRE = regexp.MustCompile(`\n!pubdate ....-..-..\b`)
 var titleRE = regexp.MustCompile(`(?:^#|\n!title) (\w+):? ([^\n]*)`)
