0697579ba28b383146afcb35be60e2d195b1c257
Balazs Kezes <3j4b0y1i@anonaddy.me>
2025-04-05 06:13:55 +0200

    frontpagejs: extract the placeholder index into a variable

--- docs/frontpage.js
+++ docs/frontpage.js
@@ -1,6 +1,7 @@
 function filter() {
   let elems = document.body.childNodes
   let tag = ""
+  let phidx = 4  // placeholder index
   if (location.hash.length >= 1) tag = location.hash.substring(1)
 
   let txt = ""
@@ -9,7 +10,7 @@ function filter() {
   txt += "<a href=#fav>@#fav</a>, or "
   txt += "<a href=#demo>@#demo</a> for the interesting posts."
   if (tag == "") {
-    document.querySelector("li:nth-child(4)").innerHTML = txt
+    document.querySelector(`li:nth-child(${phidx})`).innerHTML = txt
     for (let e of elems) e.hidden = false
     hSelection.hidden = true
     hFilterMessage.hidden = true
@@ -33,7 +34,7 @@ function filter() {
   if (tag == "demo") filterMessage = "Interactive posts:"
   if (tag == "projects") filterMessage = "My bigger entries that might be useful for others too:"
 
-  document.querySelector("li:nth-child(4)").innerHTML = txt
+  document.querySelector(`li:nth-child(${phidx})`).innerHTML = txt
   for (let i = 4; i < elems.length; i++) elems[i].hidden = true
   hSelection.hidden = false
   hFilterMessage.innerText = filterMessage
