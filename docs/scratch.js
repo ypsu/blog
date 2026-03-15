b09d536efa77de09dc732aae28778fda7c4fb7a1
Balazs Kezes <3j4b0y1i@anonaddy.me>
2026-03-15 16:46:50 +0100

    scratch: format

--- docs/scratch.js
+++ docs/scratch.js
@@ -43,7 +43,7 @@ function draw(eid, x, y) {
   if (entry.rem == 0) return
   x = clamp(x)
   y = clamp(y)
-  let ctx = entry.hcanvas.getContext('2d')
+  let ctx = entry.hcanvas.getContext("2d")
   let data = entry.img.data
   for (let j = y - radius; j <= y + radius; j++) {
     for (let i = x - radius; i <= x + radius; i++) {
@@ -59,15 +59,15 @@ function draw(eid, x, y) {
   if (entry.rem == 0) {
     entry.hrem.style.opacity = 0
     entry.hsecret.style.opacity = 1
-    entry.hcanvas.style.borderColor = 'green'
+    entry.hcanvas.style.borderColor = "green"
   }
 }
 
 function mousemove(eid, evt) {
   if ((evt.buttons & 1) != 1) return
   let hcanvas = entries[eid].hcanvas
-  let x = evt.offsetX * hcanvas.width / hcanvas.clientWidth
-  let y = evt.offsetY * hcanvas.height / hcanvas.clientHeight
+  let x = (evt.offsetX * hcanvas.width) / hcanvas.clientWidth
+  let y = (evt.offsetY * hcanvas.height) / hcanvas.clientHeight
   draw(eid, x, y)
 }
 
@@ -75,30 +75,26 @@ function touchmove(eid, evt) {
   evt.preventDefault()
   let hcanvas = entries[eid].hcanvas
   let r = hcanvas.getBoundingClientRect()
-  let x = (evt.touches[0].clientX - r.x) * gridsize / hcanvas.clientWidth
-  let y = (evt.touches[0].clientY - r.y) * gridsize / hcanvas.clientHeight
+  let x = ((evt.touches[0].clientX - r.x) * gridsize) / hcanvas.clientWidth
+  let y = ((evt.touches[0].clientY - r.y) * gridsize) / hcanvas.clientHeight
   draw(eid, x, y)
 }
 
 function escapehtml(unsafe) {
-  return unsafe.replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#039;');
+  return unsafe.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;")
 }
 
 // input: h as an angle in [0,360] and s,l in [0,1] - output: r,g,b in [0,255]
 function hsl2rgb(h, s, l) {
-  let a = s * Math.min(l, 1 - l);
-  let f = (n, k = (n + h / 30) % 12) => l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-  return [Math.floor(f(0) * 255), Math.floor(f(8) * 255), Math.floor(f(4) * 255)];
+  let a = s * Math.min(l, 1 - l)
+  let f = (n, k = (n + h / 30) % 12) => l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1)
+  return [Math.floor(f(0) * 255), Math.floor(f(8) * 255), Math.floor(f(4) * 255)]
 }
 
 function main() {
   let entry = {}
-  for (let line of textdata.split('\n')) {
-    if (line == '') continue
+  for (let line of textdata.split("\n")) {
+    if (line == "") continue
     if (entry.text == undefined) {
       entry.text = line
     } else {
@@ -108,7 +104,7 @@ function main() {
     }
   }
 
-  let h = ''
+  let h = ""
   h += `
     <style>
       canvas {
@@ -135,15 +131,15 @@ function main() {
   h += `</table>`
   hcontent.innerHTML = h
 
-  let canvases = document.getElementsByTagName('canvas')
-  let rems = document.getElementsByClassName('crem')
-  let secrets = document.getElementsByClassName('csecret')
+  let canvases = document.getElementsByTagName("canvas")
+  let rems = document.getElementsByClassName("crem")
+  let secrets = document.getElementsByClassName("csecret")
   for (let i = 0; i < entries.length; i++) {
     entries[i].hcanvas = canvases[i]
     entries[i].hrem = rems[i]
     entries[i].hsecret = secrets[i]
     entries[i].rem = gridsize * gridsize
-    let ctx = canvases[i].getContext('2d')
+    let ctx = canvases[i].getContext("2d")
     let img = ctx.getImageData(0, 0, gridsize, gridsize)
     let data = img.data
     for (let y = 0; y < gridsize; y++) {
@@ -152,22 +148,22 @@ function main() {
         let [red, green, blue] = [255, 255, 255]
         switch (i) {
           case 0:
-            [red, green, blue] = hsl2rgb(360 * x / gridsize, 0.8, lightness)
+            ;[red, green, blue] = hsl2rgb((360 * x) / gridsize, 0.8, lightness)
             break
           case 1:
-            [red, green, blue] = hsl2rgb(360 * y / gridsize, 0.8, lightness)
+            ;[red, green, blue] = hsl2rgb((360 * y) / gridsize, 0.8, lightness)
             break
           case 2:
-            [red, green, blue] = hsl2rgb(360 * (x + y) / gridsize, 0.8, lightness)
+            ;[red, green, blue] = hsl2rgb((360 * (x + y)) / gridsize, 0.8, lightness)
             break
           case 3:
-            [red, green, blue] = hsl2rgb(360 * Math.hypot(x - gridsize / 2, y - gridsize / 2) / gridsize * 2, 0.8, lightness)
+            ;[red, green, blue] = hsl2rgb(((360 * Math.hypot(x - gridsize / 2, y - gridsize / 2)) / gridsize) * 2, 0.8, lightness)
             break
           case 4:
-            [red, green, blue] = hsl2rgb(360 * (gridsize + x - y) / gridsize, 0.8, lightness)
+            ;[red, green, blue] = hsl2rgb((360 * (gridsize + x - y)) / gridsize, 0.8, lightness)
             break
           case 5:
-            [red, green, blue] = hsl2rgb(360 * (y / gridsize + Math.sin(x / gridsize * 2 * Math.PI) / 8), 0.8, lightness)
+            ;[red, green, blue] = hsl2rgb(360 * (y / gridsize + Math.sin((x / gridsize) * 2 * Math.PI) / 8), 0.8, lightness)
             break
         }
         data[j * 4 + 0] = red
@@ -181,7 +177,7 @@ function main() {
           case 3:
             reveal |= Math.floor(y / radius) % 8 == 0
           case 2:
-            reveal |= (x < 2 * radius || x >= gridsize - 2 * radius) || (y < 2 * radius || y >= gridsize - 2 * radius)
+            reveal |= x < 2 * radius || x >= gridsize - 2 * radius || y < 2 * radius || y >= gridsize - 2 * radius
           case 1:
             reveal |= (x < 2 * radius || x >= gridsize - 2 * radius) && (y < 2 * radius || y >= gridsize - 2 * radius)
         }
