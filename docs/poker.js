fa66bbbdf026c9c39f2aaed83f943a53f9ec8e90
Balazs Kezes <3j4b0y1i@anonaddy.me>
2026-03-15 18:13:19 +0100

    poker: make it CSP compliant

---
+++ docs/poker.js
@@ -0,0 +1,27 @@
+let ss = new CSSStyleSheet()
+ss.replace(`
+  .red { color: red; }
+  table {
+    border: 1px solid black;
+    cursor: default;
+    display: inline-block;
+    line-height: normal;
+  }
+  #hgrid { line-height: 0; }
+  pre { display: inline; }
+  @media screen {
+    #hgrid { display: none; }
+  }
+  @media print {
+    p, ul, hr, textarea, #htable, span:not(.red) { display: none; }
+  }
+`)
+document.adoptedStyleSheets.push(ss)
+
+const handtable = htable.innerHTML.trim();
+const gengrid = _ => {
+  let h = "";
+  for (let i = 0; i < 120; i++) h += handtable;
+  hgrid.innerHTML = h;
+};
+gengrid();
